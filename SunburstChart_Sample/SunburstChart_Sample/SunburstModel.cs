1feb26094917a1f5ff1440843fe0e76d8116fe67
AzureAD\SrideviSivakumar <AzureAD\sridevi.sivakumar@syncfusion.com>
2021-02-12 13:56:11 +0530

    XAMARIN-38746 - Getting started sample committed

---
+++ SunburstChart_Sample/SunburstChart_Sample/SunburstModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SunburstChart_Sample
+{
+    public class SunburstModel
+    {
+        public string JobDescription { get; set; }
+        public string JobGroup { get; set; }
+        public string JobRole { get; set; }
+        public double EmployeesCount { get; set; }
+        public string Country { get; set; }
+
+    }
+}
---
+++ SunburstChart_Sample/SunburstChart_Sample/SunburstViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Text;
+
+namespace SunburstChart_Sample
+{
+    public class SunburstViewModel
+    {
+        public ObservableCollection<SunburstModel> DataSource { get; set; }
+
+        public ObservableCollection<Color> CustomPalette { get; set; }
+        public SunburstViewModel()
+        {
+            CustomPalette = new ObservableCollection<Color>();
+            CustomPalette.Add(Color.Red);
+            CustomPalette.Add(Color.Blue);
+            CustomPalette.Add(Color.Green);
+            CustomPalette.Add(Color.Pink);
+
+            this.DataSource = new ObservableCollection<SunburstModel>
+            {
+                new SunburstModel { Country = "USA", JobDescription = "Sales", JobGroup="Executive", EmployeesCount = 50 },
+                new SunburstModel { Country = "USA", JobDescription = "Sales", JobGroup = "Analyst", EmployeesCount = 40 },
+                new SunburstModel { Country = "USA", JobDescription = "Marketing", EmployeesCount = 40 },
+                new SunburstModel { Country = "USA", JobDescription = "Technical", JobGroup = "Testers", EmployeesCount = 35 },
+                new SunburstModel { Country = "USA", JobDescription = "Technical", JobGroup = "Developers", JobRole = "Windows", EmployeesCount = 175 },
+                new SunburstModel { Country = "USA", JobDescription = "Technical", JobGroup = "Developers", JobRole = "Web", EmployeesCount = 70 },
+                new SunburstModel { Country = "USA", JobDescription = "Management", EmployeesCount = 40 },
+                new SunburstModel { Country = "USA", JobDescription = "Accounts", EmployeesCount = 60 },
+
+                new SunburstModel { Country = "India", JobDescription = "Technical", JobGroup = "Testers", EmployeesCount = 33 },
+                new SunburstModel { Country = "India", JobDescription = "Technical", JobGroup = "Developers", JobRole = "Windows", EmployeesCount = 125 },
+                new SunburstModel { Country = "India", JobDescription = "Technical", JobGroup = "Developers", JobRole = "Web", EmployeesCount = 60 },
+                new SunburstModel { Country = "India", JobDescription = "HR Executives", EmployeesCount = 70 },
+                new SunburstModel { Country = "India", JobDescription = "Accounts", EmployeesCount = 45 },
+
+                new SunburstModel { Country = "Germany", JobDescription = "Sales", JobGroup = "Executive", EmployeesCount = 30 },
+                new SunburstModel { Country = "Germany", JobDescription = "Sales", JobGroup = "Analyst", EmployeesCount = 40 },
+
+                new SunburstModel { Country = "Germany", JobDescription = "Marketing", EmployeesCount = 50 },
+                new SunburstModel { Country = "Germany", JobDescription = "Technical", JobGroup = "Testers", EmployeesCount = 40 },
+                new SunburstModel { Country = "Germany", JobDescription = "Technical", JobGroup = "Developers", JobRole = "Windows", EmployeesCount = 65 },
+
+                new SunburstModel { Country = "Germany", JobDescription = "Technical", JobGroup = "Developers", JobRole = "Web", EmployeesCount = 27 },
+
+                new SunburstModel { Country = "Germany", JobDescription = "Management", EmployeesCount = 33 },
+                new SunburstModel { Country = "Germany", JobDescription = "Accounts", EmployeesCount = 55 },
+
+                new SunburstModel { Country = "UK", JobDescription = "Technical", JobGroup = "Testers", EmployeesCount = 25 },
+                new SunburstModel { Country = "UK", JobDescription = "Technical", JobGroup = "Developers", JobRole = "Windows", EmployeesCount = 96 },
+                new SunburstModel { Country = "UK", JobDescription = "Technical", JobGroup = "Developers", JobRole = "Web", EmployeesCount = 55 },
+                new SunburstModel { Country = "UK", JobDescription = "HR Executives", EmployeesCount = 60 },
+                new SunburstModel { Country = "UK", JobDescription = "Accounts", EmployeesCount = 30 }
+            };
+        }
+    }
+}
